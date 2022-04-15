dd9340aa5638b30f5fd3bf1ad9028cfc0073bd37
TheBriar <pjotrvandijk@hotmail.nl>
2022-04-15 22:53:38 +0200

    use img width/height, worse for performance

--- src/graphics/draw_foreground.c
+++ src/graphics/draw_foreground.c
@@ -23,7 +23,7 @@ static void	set_color(t_data *data, t_vector_uint	tex, int x, int y)
 	int	pixel_pos;
 	int	fg_pos;
 
-	pixel_pos = (tex.y * TEXTURE_WIDTH + tex.x) * 4;
+	pixel_pos = (tex.y * data->mlx.tex[data->caster.side]->width + tex.x) * 4;
 	fg_pos = (y * data->mlx.fg->width + x) * 4;
 	ft_memcpy(data->mlx.fg->pixels + fg_pos, \
 		data->mlx.tex[data->caster.side]->pixels + pixel_pos, 4);
@@ -31,22 +31,26 @@ static void	set_color(t_data *data, t_vector_uint	tex, int x, int y)
 
 void	draw_walls(t_data *data, int x)
 {
+	const int		side = data->caster.side;
 	double			step;
 	double			tex_pos;
 	int				y;
 
-	data->caster.tex.x = (int)(data->caster.wall_x * (double)(TEXTURE_WIDTH));
+	data->caster.tex.x = (int) \
+		(data->caster.wall_x * (double)(data->mlx.tex[side]->width));
 	if (data->caster.side < NORTH && data->caster.ray_dir.x > 0)
-		data->caster.tex.x = TEXTURE_WIDTH - data->caster.tex.x - 1;
+		data->caster.tex.x = \
+			data->mlx.tex[side]->width - data->caster.tex.x - 1;
 	else if (data->caster.side > EAST && data->caster.ray_dir.y < 0)
-		data->caster.tex.x = TEXTURE_WIDTH - data->caster.tex.x - 1;
-	step = 1.0 * TEXTURE_HEIGHT / data->caster.line_height;
+		data->caster.tex.x = \
+			data->mlx.tex[side]->width - data->caster.tex.x - 1;
+	step = 1.0 * data->mlx.tex[side]->height / data->caster.line_height;
 	tex_pos = (data->caster.draw_start - SCREEN_HEIGHT / 2 + \
 		data->caster.line_height / 2) * step;
 	y = data->caster.draw_start;
 	while (y <= data->caster.draw_end)
 	{
-		data->caster.tex.y = (int)tex_pos & (TEXTURE_HEIGHT - 1);
+		data->caster.tex.y = (int)tex_pos & (data->mlx.tex[side]->height - 1);
 		tex_pos += step;
 		set_color(data, data->caster.tex, x, y);
 		y++;
